5c45374a7a67336490d9f7695037f8423d973785
sukiscm <manuelmolinaleon1@gmail.com>
2023-07-13 18:29:44 -0600

    Sunna Toc 1.2

--- src/pages/Componentes/TablaItem.jsx
+++ src/pages/Componentes/TablaItem.jsx
@@ -23,16 +23,16 @@ export const TablaItem = ({setTrigger,trigger}) => {
             )
     }
     const columns = [
-        { field: 'id', headerName: 'ID', width: 170,headerAlign: 'center',  },
-        { field: 'nombre', headerName: 'NOMBRE', width: 300,headerAlign: 'center',  },
-        { field: 'nivel', headerName: 'MODO', width: 170,headerAlign: 'center',  },
-        { field: 'origen', headerName: 'JEFE', width: 500,headerAlign: 'center', },
-        { field: 'tipo', headerName: 'CASILLA', width: 300 },
+        { field: 'id', headerName: 'ID', width: 170,headerAlign: 'center',align:'center' },
+        { field: 'nombre', headerName: 'NOMBRE', width: 300,headerAlign: 'center',align:'center' },
+        { field: 'nivel', headerName: 'MODO', width: 170,headerAlign: 'center',align:'center'},
+        { field: 'origen', headerName: 'JEFE', width: 500,headerAlign: 'center',align:'center' },
+        { field: 'tipo', headerName: 'CASILLA', width: 300,headerAlign: 'center',align:'center' },
         { field: 'ENTREGAR', headerName: 'ENTREGAR', width: 170,  renderCell: (params) => (
           <Button  variant='contained' color='warning' onClick={e=>setDialogVisibility({visibile:true,itemInfo:params.row})}>
             Entregar A:
           </Button>
-        ), },
+        ),headerAlign: 'center',align:'center' },
 
     ]
   return (
